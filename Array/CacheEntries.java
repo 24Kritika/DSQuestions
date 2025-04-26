cf409c69f890ab3439634dc5f1b88b8e47cf30e6
24Kritika <raikritika87@gmail.com>
2025-04-26 14:04:25 +0530

    New

--- Array/CacheEntries.java
+++ Array/CacheEntries.java
@@ -1,2 +1,45 @@
-package Array;public class CacheEntries {
+package Array;
+
+import java.util.*;
+
+public class CacheEntries {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int elementCount = sc.nextInt();
+        String[][] cache_entries = new String[n][elementCount];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<elementCount;j++){
+                cache_entries[i][j] = sc.next();
+            }
+        }
+        int q = sc.nextInt();
+        int q_element = sc.nextInt();
+        String[][] queries = new String[q][q_element];
+        for(int i=0;i<q;i++){
+            for(int j=0;j<q_element;j++){
+                queries[i][j] = sc.next();
+            }
+        }
+        int answer[] = getQueryAnswers(cache_entries,queries);
+        for(int res:answer){
+            System.out.println(res);
+        }
+    }
+
+    private static int[] getQueryAnswers(String[][] cache_entries,String[][] queries){
+        Map<String,ArrayList<String>> map = new HashMap<>();
+        int cache_len = cache_entries.length;
+        int q_len = queries.length;
+        int[] answer = new int[q_len];
+
+        for (String[] cacheEntry : cache_entries) {
+            map.put(cacheEntry[0], new ArrayList<String>(Arrays.asList(cacheEntry[1], cacheEntry[2])));
+        }
+
+        for(int i=0;i<q_len;i++){
+            answer[i]=Integer.parseInt(map.get(queries[i][1]).get(1));
+        }
+        return answer;
+    }
 }
--- Array/ValidWordCount.java
+++ Array/ValidWordCount.java
@@ -1,2 +1,46 @@
-package Array;public class ValidWordCount {
+package Array;
+import java.util.Scanner;
+import java.util.*;
+
+public class ValidWordCount {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String input_string = sc.nextLine();
+        System.out.println(countValidWords(input_string));
+    }
+
+    public static int countValidWords(String s){
+        int validCount = 0;
+        String[] arr = s.split(" ");
+        for(String word : arr){
+            if(isAlphaNumeric(word) && word.length()>=3 && hasVowelAndConsonant(word)){
+                validCount++;
+            }
+        }
+        return validCount;
+    }
+
+    public static boolean isAlphaNumeric(String str){
+        char[] char_arr = str.toCharArray();
+        for(char ch:char_arr){
+            if(!((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9'))){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean hasVowelAndConsonant(String str){
+        char[] char_arr = str.toCharArray();
+        ArrayList<Character> vowelList = new ArrayList<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for(char c : char_arr){
+            if(vowelList.contains(c)){
+                hasVowel=true;
+            }else if(!(c>='0' && c<='9')){
+                hasConsonant=true ;
+            }
+        }
+        return (hasVowel && hasConsonant);
+    }
 }
